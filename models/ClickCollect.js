7441d6d2f2abe286548c8673e099e9dbee4e3473
jis <jis@jisdev.com>
2025-04-04 10:50:02 +0200

    modify clickCollect model, add 'isActive'

--- models/ClickCollect.js
+++ models/ClickCollect.js
@@ -4,6 +4,10 @@ const openingHourSchema = require("./OpeningHour");
 const clickCollectSchema = mongoose.Schema(
   {
     instructions: { type: String },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
     openingHours: [openingHourSchema],
   },
   { timestamps: true },

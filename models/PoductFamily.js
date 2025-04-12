a2309ff0a418d28898668e047e8a6f2757432cac
jis <jis@jisdev.com>
2025-04-12 16:36:05 +0200

    update ProductFamily model, add suggestedTags

--- models/PoductFamily.js
+++ models/PoductFamily.js
@@ -19,6 +19,12 @@ const productFamilySchema = mongoose.Schema(
       ref: "productcategory",
       default: null,
     },
+    suggestedTags: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "tagCategories",
+      },
+    ],
   },
   { timestamps: true },
 );
--- models/TagCategory.js
+++ models/TagCategory.js
@@ -1,19 +1,22 @@
 const mongoose = require("./mongoose");
 
-const tagCategorySchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const tagCategorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    descriptio: {
+      type: String,
+      required: false,
+    },
+    color: {
+      type: String,
+    },
   },
-  descriptio: {
-    type: String,
-    required: false,
-  },
-  color: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
-const TagCategory = mongosse.model("tagCategories", tagCategorySchema);
+const TagCategory = mongoose.model("tagCategories", tagCategorySchema);
 
 module.export = TagCategory;
